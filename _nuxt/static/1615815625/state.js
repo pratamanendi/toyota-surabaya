2b7805931af46757c8e8b872f787aebe5f865601
nendi pratama <pratamanendi@gmail.com>
2021-03-15 20:41:40 +0700

    newBranch

---
+++ _nuxt/static/1615815625/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftoyota-surabaya\u002F_nuxt\u002Fstatic\u002F1615815625",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Ftoyota-surabaya\u002F",assetsPath:"\u002Ftoyota-surabaya\u002F_nuxt\u002F",cdnURL:a}}}}(null));
